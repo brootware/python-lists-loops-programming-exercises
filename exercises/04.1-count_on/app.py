d8c34b9c5169076ed063b7b58da997387e372b52
sysadmin <sysadmin@rangemint.com>
2025-12-12 07:19:56 +0000

    finished type checking

--- exercises/04.1-count_on/app.py
+++ exercises/04.1-count_on/app.py
@@ -1,3 +1,10 @@
 my_list = [42, True, "towel", [2,1], 'hello', 34.4, {"name": "juan"}]
 
 # Your code here
+new_list = []
+
+for elm in my_list:
+    if type(elm) == dict or type(elm) == list:
+        new_list.append(elm)
+
+print(new_list)
