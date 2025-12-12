44d9fd2557345f171a8899eb1457f0ff58f53526
sysadmin <sysadmin@rangemint.com>
2025-12-12 06:55:12 +0000

    saving progress

--- exercises/01.5-Add-item-to-list/app.py
+++ exercises/01.5-Add-item-to-list/app.py
@@ -3,3 +3,9 @@
 my_list = [4, 5, 734, 43, 45]
 
 # The magic goes below
+import random
+for i in range(10):
+    my_random_int = random.randint(1,10)
+    my_list.append(my_random_int)
+
+print(my_list)
