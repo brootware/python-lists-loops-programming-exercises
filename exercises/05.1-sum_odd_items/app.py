c263d22f702a42dbfa1251027e8f601b76f6dd17
bruce <bruce@workmac.local.com>
2025-12-14 06:27:22 +0000

    fixed solution for sum odds

--- exercises/05.1-sum_odd_items/app.py
+++ exercises/05.1-sum_odd_items/app.py
@@ -1,12 +1,12 @@
 my_list = [4,5,734,43,45,100,4,56,23,67,23,58,45]
 
 # Your code here
-def sum_odd_nums:(list):
-    total = 0
-    for num in my_list:
-        if num % 2 != 0:
-            total += num
-    
-    return total
+def sum_odds(list):
+  total = 0
+  for num in my_list:
+    if num % 2 != 0:
+      total += num
+  
+  return total
 
-print(sum_odd_nums(my_list))
+print(sum_odds(my_list))
